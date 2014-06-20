ef322cc43de800952e6785831eb496a22eec8f6a
Ignacio Tolosa <ignacio.tolosa.12@sansano.usm.cl>
2014-06-20 01:46:23 -0400

    Use dequeue in job instance

--- lib/active_job/dequeuing.rb
+++ lib/active_job/dequeuing.rb
@@ -3,7 +3,7 @@ require 'active_job/arguments'
 module ActiveJob
   module Dequeuing
     extend ActiveSupport::Concern
-    module ClassMethods
+
       # Dequeue an existent job
       #
       #   dequeue(user_id)
@@ -11,6 +11,6 @@ module ActiveJob
       def dequeue(*args, &blk)
         queue_adapter.dequeue self, *args, &blk
       end
-    end
+
   end
 end
